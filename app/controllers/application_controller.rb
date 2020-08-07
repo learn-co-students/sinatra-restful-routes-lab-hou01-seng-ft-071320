c7944cc38747ec39dc4673b93b213ba38074f604
dscottcole <67556122+dscottcole@users.noreply.github.com>
2020-08-07 11:51:13 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,60 @@
 class ApplicationController < Sinatra::Base
   configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
+    set :public_folder, "public"
+    set :views, "app/views"
   end
 
   # code actions here!
+
+  get "/" do
+    redirect to "/recipes"
+  end
+
+  get "/recipes" do
+    @recipes = Recipe.all
+    erb :index
+  end
+  
+  get "/recipes/new" do
+    erb :new
+  end
+
+  post "/recipes" do
+    @recipe = Recipe.create(params)
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  get "/recipes/:id" do
+    if Recipe.all.find_by(id: params[:id]).nil?
+      redirect to "/recipes"
+    else
+      @recipe = Recipe.all.find_by(id: params[:id])
+      erb :show
+    end
+  end
+
+  get "/recipes/:id/edit" do
+    if Recipe.all.find_by(id: params[:id]).nil?
+      redirect to "/recipes"
+    else
+      @recipe = Recipe.all.find_by(id: params[:id])
+      erb :edit
+    end
+  end
+
+  patch "/recipes/:id" do
+    @recipe = Recipe.all.find_by(id: params[:id])
+    @recipe.update(params[:recipe])
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  delete "/recipes/:id" do
+    Recipe.all.destroy(params[:id])
+    redirect to "/recipes"
+  end
+
+
+
+
 
 end
